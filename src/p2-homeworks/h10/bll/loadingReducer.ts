e688e5649eb7a2aae8dc252704362fcd854def37
KseniaTst <98537255+KseniaTst@users.noreply.github.com>
2022-05-11 15:52:41 +0300

    Initialize project using Create React App

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,22 @@
-const initState = {
-
+export type InitType={
+    isload:boolean
+}
+export type LoadingAT={
+    type: 'LOADING'
+    isload:boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+const initState = {
+isload:false
+}
+
+export const loadingReducer = (state = initState, action: LoadingAT): InitType => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'LOADING': {
+          return {...state , isload: action.isload}
         }
         default: return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (isload:boolean): LoadingAT => ({ type:'LOADING' as const ,isload}) // fix any
